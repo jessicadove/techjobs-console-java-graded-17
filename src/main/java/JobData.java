5ae54ab5900d2aebaf45d8680af39ce78b817f9a
Jessica <jlknobeloch@gmail.com>
2023-08-25 21:23:41 -0500

    added case sensitivity to user search terms

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -75,7 +75,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -99,7 +99,7 @@ public class JobData {
         for (HashMap<String, String> row : allJobs) {
             for (String key : row.keySet()) {
                 String aValue = row.get(key);
-                if (aValue.contains(value)) {
+                if (aValue.toLowerCase().contains(value.toLowerCase())) {
                     jobs.add(row);
                     break;
                 }
